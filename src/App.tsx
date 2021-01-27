d73809853328376d950d66fcd09af4448a5c9089
timothyliutl <18tfl1@queensu.ca>
2021-01-27 03:42:41 -0500

    playing around with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./Pages/Welcome"
+import AppBar from 'material-ui/AppBar'
+import Typography from '@material-ui/core/Typography'
+import Grid from "@material-ui/core/Grid"
+
+interface type {
+readonly name: number 
+}
+const name:type = {
+  name: 69
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant={'h1'}>Hello There</Typography>
     </div>
   );
 }
