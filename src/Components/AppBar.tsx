164272da9de56ce0e3c458c38029ecb9a042c3d5
timothyliutl <18tfl1@queensu.ca>
2021-02-11 01:34:30 -0500

    styling edits

--- src/Components/AppBar.tsx
+++ src/Components/AppBar.tsx
@@ -1,41 +1,51 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
-import { Toolbar } from '@material-ui/core';
+import { Button, Grid, Toolbar } from '@material-ui/core';
 import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    page:{
-        marginTop:100
-    },
-    appbar:{
-        position:'fixed',
-        borderRadius:16
-    }
-  }),
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flex: 1
+        },
+        page: {
+            marginTop: 100,
+            width: '70%'
+        },
+        appbar: {
+            position: 'fixed',
+            borderRadius: 16
+        }
+    }),
 );
 
 const Appbar = () => {
-    //TODO: get rid of the type any
 
-const classes = useStyles();
+    const classes = useStyles();
 
     return (
-        
-        
+
+
         <div className={classes.page}>
             <AppBar className={classes.appbar}>
                 <Toolbar>
-                    <Typography></Typography>
+                    <Grid container justify={'space-evenly'}>
+                        <Grid item>
+                            <Typography variant={'h6'}>Home</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant={'h6'}>Current Projects</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant={'h6'}>Contact</Typography>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </div>
