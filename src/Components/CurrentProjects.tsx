41e688f6d18e60aa66400ac7c1341aaf869b50d0
timothyliutl <18tfl1@queensu.ca>
2021-02-19 23:40:43 -0500

    added link tree

--- src/Components/CurrentProjects.tsx
+++ src/Components/CurrentProjects.tsx
@@ -1,4 +1,5 @@
 import { Grid, makeStyles } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import ProjectCards from './ProjectCards';
 
@@ -13,6 +14,10 @@ const useStyles = makeStyles({
         div:{
             width:"80%",
             
+        },
+        title:{
+            margin:50,
+            
         }
     });
 
@@ -22,6 +27,7 @@ const CurrentProjects = () =>{
     const classes = useStyles()
     return(
         <div className={classes.div}>
+            <Typography className={classes.title} variant={'h2'}>Current Projects</Typography>
             <Grid container justify={'space-between'}>
                 <Grid item className={classes.gridListItem}>
                     <ProjectCards projectName={'Hello'} projectDescription={'hello'} projectImage={'https://cdn.frankerfacez.com/avatar/twitch/406440657'}></ProjectCards>
--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,29 +1,83 @@
-import { Container, Grid, Typography } from '@material-ui/core';
+import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
 import Card from '@material-ui/core/Card'
-import React, { FC } from 'react'; 
+import React, { FC } from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 
 
-interface IProps{
+interface IProps {
     cards?: JSON
 }
 
-const Welcome:FC<IProps> = (props) =>{
+const Welcome: FC<IProps> = (props) => {
+    const styles = makeStyles({
+        card: {
+            widthMin: 200,
+            height: 75,
+            padding: 20,
+
+        },
+        title:{
+            marginTop:30
+        },
+        type: {
+            marginBottom: 30,
+        },
+        center: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        icon: {
+            marginLeft: 20,
+            marginRight: 20
+        },
+        link: {
+            textDecoration: 'none',
+
+        }
+    });
+    const classes = styles();
     const cards = props.cards;
 
-    return(
+    return (
         <div>
-            <Typography variant={'h4'}>Hello, I am Timothy Liu</Typography>
-            <Typography variant={'h6'}>
+            <Typography variant={'h2'}>Hello, I am Timothy Liu</Typography>
+            <Typography variant={'h5'} className={classes.type}>
                 Second year Queen's Mathematics and Engineering Student
             </Typography>
-            <Card>
-                <Grid container direction={'row'}>
-                    <Container>
-                        <Typography variant={'h6'}></Typography>
-                    </Container>
-                </Grid>
-            </Card>
-          
+
+            <Typography variant={'h2'} className={classes.title}>
+                Quick Links
+            </Typography>
+            <Grid container justify={'center'}>
+                <Card className={classes.card}>
+                    <Grid container direction={'row'}>
+                        <Grid item className={classes.icon}>
+                            <a className={classes.link} href={'https://drive.google.com/file/d/1aWGhG6A5G0cX_RKIhh_stYlnLnjjoHA0/view?usp=sharing'}>
+                            <InsertDriveFileIcon fontSize={'large'} color={'inherit'}></InsertDriveFileIcon>
+                            <Typography variant={'h6'}>Resume</Typography>
+                            </a>
+                        </Grid>
+                        <Grid item className={classes.icon}>
+                            <a className={classes.link} href={'https://github.com/timothyliutl'}><GitHubIcon fontSize={'large'}></GitHubIcon>
+                                <Typography variant={'h6'}>GitHub</Typography></a>
+                        </Grid>
+                        <Grid item className={classes.icon}>
+                            <a href={'https://www.linkedin.com/in/timothy-liu-4a738619b/'} className={classes.link}>
+                                <LinkedInIcon fontSize={'large'} color={'primary'}></LinkedInIcon>
+                                <Typography variant={'h6'}>LinkedIn</Typography>
+                            </a>
+
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Grid>
+
+
+
+
         </div>
     )
 }
