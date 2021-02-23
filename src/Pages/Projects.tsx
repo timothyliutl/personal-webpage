9a42cca460c2a56823283980ccb48b56739173f9
timothyliutl <18tfl1@queensu.ca>
2021-02-23 18:29:34 -0500

    incase npm packages are messed up

---
+++ src/Pages/Projects.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FC } from 'react';
+
+interface iProps{
+
+}
+
+const Projects:FC<iProps> = (props:iProps) => {
+    return(
+    <div>
+        
+    </div>
+    )
+}
+
+export default Projects;
