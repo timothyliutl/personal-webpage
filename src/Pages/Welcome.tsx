eba447b741c51b0957fee1e14b915661db3a451b
timothyliutl <18tfl1@queensu.ca>
2021-05-13 15:40:24 -0400

    removed hover feature for now

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -4,6 +4,7 @@ import React, { FC, useState } from 'react';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import  Paper  from '@material-ui/core/Paper';
 
 
 interface IProps {
@@ -14,15 +15,13 @@ interface IProps {
 const Welcome: FC<IProps> = (props) => {
     const styles = makeStyles({
         card: {
-            widthMin: 200,
-            heightMin: 75,
             padding: 20,
-            marginTop: 15,
-            borderRadius:16
+            margin: 15,
+            borderRadius: 16
 
         },
-        title:{
-            marginTop:30
+        title: {
+            marginTop: 30
         },
         type: {
             marginBottom: 15,
@@ -42,28 +41,47 @@ const Welcome: FC<IProps> = (props) => {
 
         },
 
-        individualCards:{
-            padding:5,
-            borderRadius:10
+        individualCards: {
+            
+            padding: 10,
+            borderRadius: 10
         }
     });
     const classes = styles();
     const cards = props.cards;
-    
-    const [hoverID,SetId] = useState(" ");
-    const [isHovering, setIsHovering] = useState(false);
 
-    const onEnter = (event:any) =>{
-        SetId(event.target.id);
+    const [hoverID, SetId] = useState(" ");
+    const [isHoveringR, toggleIsHoveringR] = useState(false);
+    const [isHoveringG, toggleIsHoveringG] = useState(false);
+    const [isHoveringL, toggleIsHoveringL] = useState(false);
+    const toggleHover = (event: any) => {
+        if(event.target.id=='linkedin'){
+            toggleIsHoveringL(false)
+        }
+        if(event.target.id=='resume'){
+            toggleIsHoveringR(false)
+        }
+        if(event.target.id=='github'){
+            toggleIsHoveringG(false)
+        }
 
     }
-    const onLeave = (event:any) =>{
-            SetId("jghkhkj")
+    const onOverHover = (event: any) => {
+        if(event.target.id=='linkedin'){
+            toggleIsHoveringL(true)
+        }
+        if(event.target.id=='resume'){
+            toggleIsHoveringR(true)
+        }
+        if(event.target.id=='github'){
+            toggleIsHoveringG(true)
+        }
 
     }
-   
-
     
+
+
+
 
     return (
         <div>
@@ -72,36 +90,36 @@ const Welcome: FC<IProps> = (props) => {
                 Second year Queen's Mathematics and Engineering Student
             </Typography>
 
-            
+
             <Grid container justify={'center'}>
-                <Card className={classes.card} elevation={1} onMouseLeave={onLeave}>
-                <Typography variant={'h4'} className={classes.type}>
-                Quick Links
-                </Typography>
+                <Card className={classes.card} elevation={1}>
+                    <Typography variant={'h4'} className={classes.type}>
+                        Quick Links
+                    </Typography>
                     <Grid container direction={'row'}>
                         <Grid item className={classes.icon}>
-                            <Card className={classes.individualCards} onMouseEnter={onEnter} onMouseLeave={onLeave} id = {'resume'} elevation={hoverID=='resume'?6:0}>
-                                <a className={classes.link} onClick={()=>{window.open('https://drive.google.com/file/d/1aWGhG6A5G0cX_RKIhh_stYlnLnjjoHA0/view')}}>
-                                <InsertDriveFileIcon fontSize={'large'} color={'inherit'}></InsertDriveFileIcon>
-                                <Typography variant={'h6'}>Resume</Typography>
+                            <Paper className={classes.individualCards} onMouseEnter={onOverHover} onMouseLeave={toggleHover} id={'resume'} elevation={ 0}>
+                                <a className={classes.link} onClick={() => { window.open('https://drive.google.com/file/d/1aWGhG6A5G0cX_RKIhh_stYlnLnjjoHA0/view') }}>
+                                    <InsertDriveFileIcon fontSize={'large'} color={'inherit'}></InsertDriveFileIcon>
+                                    <Typography variant={'h6'}>Resume</Typography>
+                                </a>
+                            </Paper>
+                        </Grid>
+                        
+                        <Grid item className={classes.icon} >
+                            <Card className={classes.individualCards} onMouseEnter={onOverHover} onMouseLeave={toggleHover} id={'github'} elevation={0}>
+                                <a className={classes.link} onClick={() => { window.open('https://github.com/timothyliutl') }}><GitHubIcon fontSize={'large'}></GitHubIcon>
+                                    <Typography variant={'h6'}>GitHub</Typography>
                                 </a>
                             </Card>
                         </Grid>
                         
                         <Grid item className={classes.icon}>
-                            <Card className={classes.individualCards} onMouseEnter={onEnter} onMouseLeave={onLeave} id = {'github'} elevation={hoverID=='github'?6:0}>
-                               <a className={classes.link} onClick={()=>{window.open('https://github.com/timothyliutl')}}><GitHubIcon fontSize={'large'}></GitHubIcon>
-                                <Typography variant={'h6'}>GitHub</Typography>
-                                </a> 
-                            </Card>
-                            
-                        </Grid>
-                        <Grid item className={classes.icon}>
-                            <Card className={classes.individualCards} onMouseEnter={onEnter} onMouseLeave={onLeave} id = {'linkedin'} elevation={hoverID=='linkedin'?6:0}>
-                            <a onClick={()=>{window.open('https://www.linkedin.com/in/timothy-liu-4a738619b/')}} className={classes.link}>
-                                <LinkedInIcon fontSize={'large'} color={'primary'}></LinkedInIcon>
-                                <Typography variant={'h6'}>LinkedIn</Typography>
-                            </a>
+                            <Card className={classes.individualCards} onMouseEnter={onOverHover} onMouseLeave={toggleHover} id={'linkedin'} elevation={ 0}>
+                                <a onClick={() => { window.open('https://www.linkedin.com/in/timothy-liu-4a738619b/') }} className={classes.link}>
+                                    <LinkedInIcon fontSize={'large'} color={'primary'}></LinkedInIcon>
+                                    <Typography variant={'h6'}>LinkedIn</Typography>
+                                </a>
                             </Card>
                         </Grid>
                     </Grid>
